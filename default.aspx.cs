75a1ec2afa5882aeaae4025fba6c95ed46459540
HASANAVCI <34420394+HASANAVCI@users.noreply.github.com>
2018-01-12 11:02:41 +0200

    Amin Panel
    Admin Panel

---
+++ default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Eticaret.admin
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        sqlbaglanti baglan = new sqlbaglanti();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_giris_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand cmd = new SqlCommand("Select * from Yonetici where yoneticiKullanici='"+txt_kullanici.Text +"'and yoneticiSifre='"+txt_sifre.Text+"'",baglan.baglan());
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                Response.Redirect("adminpanel.aspx");
+            }
+            else
+            {
+                lbl_bilgi.Text = "Hatalı Giriş Yaptınız!";
+            }
+        }
+    }
+}
